22cf9dfdc59d31f94e328d4759efca8c6681f03a
James Paolantonio <james@howaboutwe.com>
2012-07-18 11:46:31 -0400

    Adde in initial files

---
+++ JPButtonGroup.h
@@ -0,0 +1,13 @@
+//
+//  JPButtonGroup.h
+//  HowAboutWeHD
+//
+//  Created by James on 7/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JPButtonGroup : NSObject
+
+@end
---
+++ JPButtonGroup.m
@@ -0,0 +1,13 @@
+//
+//  JPButtonGroup.m
+//  HowAboutWeHD
+//
+//  Created by James on 7/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "JPButtonGroup.h"
+
+@implementation JPButtonGroup
+
+@end
